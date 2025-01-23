c7782718b79042565c76875798d506e6d300f47c
Matt Beanland <beanland@live.com.au>
2025-01-23 22:01:15 +1100

    specification resource read actions

--- lib/diffo/provider.ex
+++ lib/diffo/provider.ex
@@ -12,6 +12,8 @@ defmodule Diffo.Provider do
     resource Diffo.Provider.Specification do
       define :create_specification, action: :create
       define :get_specification_by_id, action: :read, get_by: :id
+      define :get_latest_specification_by_name, action: :get_latest
+      define :find_specifications_by_name, action: :find
       define :describe_specification, action: :describe
       define :next_minor_specification, action: :next_minor
       define :next_patch_specification, action: :next_patch
--- test/provider/specification_test.exs
+++ test/provider/specification_test.exs
@@ -15,8 +15,8 @@ defmodule Diffo.Provider.Specification_Test do
   end
 
   test "create a service specification - success - name and type supplied" do
-    {:ok, specification} = Diffo.Provider.create_specification(%{name: "adslAccess", type: :serviceSpecification})
-    assert specification.name == "adslAccess"
+    {:ok, specification} = Diffo.Provider.create_specification(%{name: "broadband", type: :serviceSpecification})
+    assert specification.name == "broadband"
     assert specification.type == :serviceSpecification
   end
 
@@ -31,7 +31,7 @@ defmodule Diffo.Provider.Specification_Test do
 
   test "create a service specification - success - name and id supplied" do
     uuid = UUID.uuid4()
-    {:ok, specification} = Diffo.Provider.create_specification(%{name: "adslAccess", id: uuid})
+    {:ok, specification} = Diffo.Provider.create_specification(%{name: "siteConnection", id: uuid})
     assert specification.id == uuid
 
     {:ok, loaded_specification} = Diffo.Provider.get_specification_by_id(uuid, load: :href)
@@ -54,37 +54,59 @@ defmodule Diffo.Provider.Specification_Test do
   end
 
   test "create a service specification - failure - id not uuidv4" do
-    {:error, _specification} = Diffo.Provider.create_specification(%{name: "adslAccess", id: "123"})
+    {:error, _specification} = Diffo.Provider.create_specification(%{name: "device", id: "123"})
   end
 
   test "create a service specification - failure - type not correct" do
-    {:error, _specification} = Diffo.Provider.create_specification(%{name: "adslAccess", type: :service})
+    {:error, _specification} = Diffo.Provider.create_specification(%{name: "aggregation", type: :service})
+  end
+
+  test "create a service specification - failure - name and major version not unique" do
+    {:ok, _specification} = Diffo.Provider.create_specification(%{name: "voice"})
+    {:error, _specification} = Diffo.Provider.create_specification(%{name: "voice"})
   end
 
   test "update the description of a specification - success" do
-    {:ok, specification} = Diffo.Provider.create_specification(%{name: "adslAccess", description: "ADSL Access Service"})
+    {:ok, specification} = Diffo.Provider.create_specification(%{name: "adsl", description: "ADSL Access Service"})
     assert specification.description == "ADSL Access Service"
     {:ok, updated_specification} = specification |> Diffo.Provider.describe_specification(%{description: "Asymmetric DSL Access Service"})
     assert updated_specification.description == "Asymmetric DSL Access Service"
   end
 
   test "make a new patch version - success" do
-    {:ok, specification} = Diffo.Provider.create_specification(%{name: "adslAccess", description: "ADSL Access Service"})
+    {:ok, specification} = Diffo.Provider.create_specification(%{name: "connectivity"})
     {:ok, updated_specification} = specification |> Diffo.Provider.next_patch_specification(load: :version)
     assert updated_specification.version == "v1.0.1"
   end
 
   test "make a new minor version, resetting the patch version - success" do
-    {:ok, specification} = Diffo.Provider.create_specification(%{name: "adslAccess", description: "ADSL Access Service"})
+    {:ok, specification} = Diffo.Provider.create_specification(%{name: "security"})
     {:ok, patched_specification} = specification |> Diffo.Provider.next_patch_specification(load: :version)
     {:ok, updated_specification} = patched_specification |> Diffo.Provider.next_minor_specification(load: :version)
     assert updated_specification.version == "v1.1.0"
   end
 
   test "make a new patch on an minor version - success" do
-    {:ok, specification} = Diffo.Provider.create_specification(%{name: "adslAccess", description: "ADSL Access Service"})
+    {:ok, specification} = Diffo.Provider.create_specification(%{name: "monitoring"})
     {:ok, updated_specification} = specification |> Diffo.Provider.next_minor_specification(load: :version)
     {:ok, patched_specification} = updated_specification |> Diffo.Provider.next_patch_specification(load: :version)
     assert patched_specification.version == "v1.1.1"
+  end
+
+  test "find specifications by name" do
+    {:ok, _v1} = Diffo.Provider.create_specification(%{name: "transport"})
+    {:ok, _v2} = Diffo.Provider.create_specification(%{name: "transport", major_version: 2})
+    {:ok, _v3} = Diffo.Provider.create_specification(%{name: "transport", major_version: 3})
+    {:ok, specifications} = Diffo.Provider.find_specifications_by_name(%{name: "transport"})
+    assert length(specifications) == 3
+  end
+
+  test "get latest specification" do
+    {:ok, _v1} = Diffo.Provider.create_specification(%{name: "edge"})
+    {:ok, _v2} = Diffo.Provider.create_specification(%{name: "edge", major_version: 2})
+    {:ok, _v3} = Diffo.Provider.create_specification(%{name: "edge", major_version: 3})
+    {:ok, latest} = Diffo.Provider.get_latest_specification_by_name(%{name: "edge"}, load: :version)
+    assert latest.major_version == 3
+    assert latest.version == "v3.0.0"
   end
 end
