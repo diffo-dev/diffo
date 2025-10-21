5615735ca620ec14f1ef85a192e1ff243e2f4b8e
Matt Beanland <beanland@live.com.au>
2025-10-21 16:38:58 +1030

    remove access domain, adapting to test assigner

---
+++ lib/diffo/provider/components/instance/extension/action_helper.ex
@@ -0,0 +1,42 @@
+# SPDX-FileCopyrightText: 2025 diffo contributors <https://github.com/diffo-dev/diffo/graphs.contributors>
+#
+# SPDX-License-Identifier: MIT
+
+defmodule Diffo.Provider.Instance.ActionHelper do
+  @moduledoc """
+  Diffo - TMF Service and Resource Management with a difference
+
+  ActionHelper - helping with Instance actions
+  """
+
+  alias Diffo.Provider.Instance.Specification
+  alias Diffo.Provider.Instance.Relationship
+  alias Diffo.Provider.Instance.Feature
+  alias Diffo.Provider.Instance.Characteristic
+  alias Diffo.Provider.Instance.Place
+  alias Diffo.Provider.Instance.Party
+
+  @doc """
+  build before_action helper, injects instance dsl configuration into the changeset
+  """
+  def build_before(changeset) do
+    changeset
+    |> Specification.set_specified_by_argument()
+    |> Feature.set_features_argument()
+    |> Characteristic.set_characteristics_argument()
+  end
+
+  @doc """
+  build after_action helper, relates TMF entities to the new instance
+  """
+  def build_after(changeset, result, module, function) do
+    with {:ok, result} <- Specification.relate_instance(result, changeset),
+         {:ok, result} <- Relationship.relate_instance(result, changeset),
+         {:ok, result} <- Feature.relate_instance(result, changeset),
+         {:ok, result} <- Characteristic.relate_instance(result, changeset),
+         {:ok, result} <- Place.relate_instance(result, changeset),
+         {:ok, result} <- Party.relate_instance(result, changeset),
+         {:ok, result} <- apply(module, function, [result.id]),
+         do: {:ok, result}
+  end
+end
