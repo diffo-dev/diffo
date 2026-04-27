7386b03f2019cd9907c76fc355c84421a6908521
Matt Beanland <beanland@live.com.au>
2026-04-27 22:05:32 +0930

    verifiers for instance extension

---
+++ lib/diffo/provider/components/instance/extension/verifiers/verify_specification.ex
@@ -0,0 +1,35 @@
+# SPDX-FileCopyrightText: 2025 diffo contributors <https://github.com/diffo-dev/diffo/graphs.contributors>
+#
+# SPDX-License-Identifier: MIT
+
+defmodule Diffo.Provider.Instance.Extension.Verifiers.VerifySpecification do
+  @moduledoc "Verifies that the specification id is a valid UUID4"
+  use Spark.Dsl.Verifier
+
+  alias Spark.Dsl.Verifier
+  alias Spark.Error.DslError
+
+  @impl true
+  def verify(dsl_state) do
+    resource = Verifier.get_persisted(dsl_state, :module)
+    spec_id = Verifier.get_option(dsl_state, [:specification], :id)
+
+    errors =
+      if spec_id && !Diffo.Uuid.uuid4?(spec_id) do
+        [
+          DslError.exception(
+            module: resource,
+            path: [:specification, :id],
+            message: "specification: id must be a valid UUID4"
+          )
+        ]
+      else
+        []
+      end
+
+    case errors do
+      [] -> :ok
+      errors -> {:error, errors}
+    end
+  end
+end
