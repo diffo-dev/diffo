748a85509ac254a27f6817ce0726a14aaf85dbd0
Matt Beanland <beanland@live.com.au>
2026-05-06 23:17:24 +0930

    release 0.2.1

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Diffo.MixProject do
   @moduledoc false
   use Mix.Project
 
-  @version "0.2.0"
+  @version "0.2.1"
   @name "Diffo"
   @description "TMF Service and Resource Manager with a difference"
   @github_url "https://github.com/diffo-dev/diffo"
