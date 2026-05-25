62caebb12821482e15aace00c733e1e9668ea3ea
Matt Beanland <beanland@live.com.au>
2026-05-25 20:58:28 +0930

    location and bounds

--- lib/diffo/provider/components/place.ex
+++ lib/diffo/provider/components/place.ex
@@ -4,7 +4,13 @@
 
 defmodule Diffo.Provider.Place do
   @moduledoc """
-  Ash Resource for a TMF Place
+  Ash Resource for a TMF Place.
+
+  For `:GeographicLocation` records the JSON encoder folds `location` /
+  `bounds` into a TMF675 `geoJson.geometry.{type, coordinates}` payload and
+  promotes the existing `@type` (the base TMF class) to `@baseType`, deriving
+  a concrete `@type` of `GeoJsonPoint` or `GeoJsonPolygon` from which
+  attribute is populated.
   """
   use Ash.Resource, fragments: [Diffo.Provider.BasePlace], domain: Diffo.Provider
 
@@ -14,12 +20,61 @@ defmodule Diffo.Provider.Place do
   end
 
   jason do
-    pick [:id, :href, :name, :referred_type, :type]
+    pick [:id, :href, :name, :referred_type, :type, :location, :bounds]
     compact true
     rename referred_type: "@referredType", type: "@type"
+    customize &Diffo.Provider.Place.encode_geo_json/2
   end
 
   outstanding do
     expect [:id, :name, :referred_type, :type]
+  end
+
+  @doc false
+  def encode_geo_json(result, record) do
+    case {record.location, record.bounds} do
+      {nil, nil} ->
+        result
+
+      {%Bolty.Types.Point{x: lon, y: lat}, nil} ->
+        result
+        |> List.keydelete(:location, 0)
+        |> List.keydelete(:bounds, 0)
+        |> rebrand_type("GeoJsonPoint")
+        |> List.keystore("geoJson", 0,
+          {"geoJson", %{geometry: %{type: "Point", coordinates: [lon, lat]}}}
+        )
+
+      {nil, %AshNeo4j.Type.Box{sw: sw, ne: ne}} ->
+        ring = [
+          [sw.x, sw.y],
+          [ne.x, sw.y],
+          [ne.x, ne.y],
+          [sw.x, ne.y],
+          [sw.x, sw.y]
+        ]
+
+        result
+        |> List.keydelete(:location, 0)
+        |> List.keydelete(:bounds, 0)
+        |> rebrand_type("GeoJsonPolygon")
+        |> List.keystore("geoJson", 0,
+          {"geoJson", %{geometry: %{type: "Polygon", coordinates: [ring]}}}
+        )
+    end
+  end
+
+  defp rebrand_type(result, concrete) do
+    case List.keyfind(result, "@type", 0) do
+      {"@type", _} ->
+        result
+        |> List.keyreplace("@type", 0, {"@type", concrete})
+        |> List.keystore("@baseType", 0, {"@baseType", "GeographicLocation"})
+
+      nil ->
+        result
+        |> List.keystore("@baseType", 0, {"@baseType", "GeographicLocation"})
+        |> List.keystore("@type", 0, {"@type", concrete})
+    end
   end
 end
